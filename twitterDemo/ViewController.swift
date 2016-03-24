e4eb1806fba678a41ace1460c67a021115da8c95
Victor Fernandez <victoraguirre.f@gmail.com>
2016-03-24 22:53:32 +0700

    Initial Commit

---
+++ twitterDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitterDemo
+//
+//  Created by victor aguirre on 3/24/16.
+//  Copyright © 2016 victor aguirre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
